82ba531c567d4369ec19b1643e386a35d314f72d
mkalkbrenner <mkalkbrenner@124705.no-reply.drupal.org>
2020-06-04 16:06:12 +0200

    Issue #3085196 by mkalkbrenner: support solarium 6

--- src/Solarium/EventDispatcher/DrupalPsr14Bridge.php
+++ src/Solarium/EventDispatcher/DrupalPsr14Bridge.php
@@ -2,21 +2,13 @@
 
 namespace Drupal\search_api_solr\Solarium\EventDispatcher;
 
-use Symfony\Component\EventDispatcher\EventDispatcherInterface;
+use Drupal\Component\EventDispatcher\ContainerAwareEventDispatcher;
+use Symfony\Component\EventDispatcher\Event;
 
 /**
  * A helper to decorate the legacy EventDispatcherInterface::dispatch().
- *
- * @method dispatch(...$args)
- * @method addListener(...$args)
- * @method addSubscriber(...$args)
- * @method removeListener(...$args)
- * @method removeSubscriber(...$args)
- * @method getListeners(...$args)
- * @method getListenerPriority(...$args)
- * @method hasListeners(...$args)
  */
-final class DrupalPsr14Bridge implements EventDispatcherInterface {
+final class DrupalPsr14Bridge extends ContainerAwareEventDispatcher {
 
   /**
    * @var \Drupal\Component\EventDispatcher\ContainerAwareEventDispatcher
@@ -27,14 +19,10 @@ final class DrupalPsr14Bridge implements EventDispatcherInterface {
     $this->dispatcher = \Drupal::service('event_dispatcher');
   }
 
-  /**
-   * Proxies all method calls to the original event dispatcher.
-   */
-  public function __call($method, $args) {
-    if ('dispatch' === $method && \is_object($args[0])) {
-      return $this->dispatcher->dispatch(\get_class($args[0]), $args[0]);
+  public function dispatch($event, Event $null = NULL) {
+    if (\is_object($event)) {
+      return $this->dispatcher->dispatch(\get_class($event), $event);
     }
-
-    return $this->dispatcher->{$method}(...$args);
+    return $this->dispatcher->dispatch($event, $null);
   }
 }
