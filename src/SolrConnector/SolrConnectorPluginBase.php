b2e061e8d18be54e8ed1fa95a106ef7c67b54c8c
Alexey Murz Korepov <murznn@gmail.com>
2021-07-29 11:35:51 +0300

    Shorten message in logs

--- src/SolrConnector/SolrConnectorPluginBase.php
+++ src/SolrConnector/SolrConnectorPluginBase.php
@@ -1003,7 +1003,7 @@ abstract class SolrConnectorPluginBase extends ConfigurablePluginBase implements
       default:
         $description = 'unreachable or returned unexpected response code';
     }
-    throw new SearchApiSolrException(sprintf('Solr endpoint %s %s (response code: %d, exception: "%s"). %s', $this->getEndpointUri($endpoint), $description, $response_code, $e->getMessage(), $body), $response_code, $e);
+    throw new SearchApiSolrException(sprintf('Solr endpoint %s %s (code: %d, message: %s). %s', $this->getEndpointUri($endpoint), $description, $response_code, $e->getMessage(), $body), $response_code, $e);
   }
 
   /**
