07b1834ca9c9a8d2ec3bbf3725d570459218440c
Markus Kalkbrenner <git@kalki.de>
2021-12-16 15:49:21 +0100

    fixed deprecation version numbers

--- src/Plugin/search_api_autocomplete/suggester/Suggester.php
+++ src/Plugin/search_api_autocomplete/suggester/Suggester.php
@@ -4,7 +4,7 @@ namespace Drupal\search_api_solr\Plugin\search_api_autocomplete\suggester;
 
 use Drupal\search_api_solr_autocomplete\Plugin\search_api_autocomplete\suggester\Suggester as SuggesterOriginal;
 
-@trigger_error('The ' . __NAMESPACE__ . '\Suggester is deprecated in search_api_solr:4.3.0 and is removed from search_api_solr:5.0.0. Instead use \Drupal\search_api_solr_autocomplete\Plugin\search_api_autocomplete\suggester\Suggester. See https://www.drupal.org/node/3254186.', E_USER_DEPRECATED);
+@trigger_error('The ' . __NAMESPACE__ . '\Suggester is deprecated in search_api_solr:4.2.4 and is removed from search_api_solr:4.3.0. Instead use \Drupal\search_api_solr_autocomplete\Plugin\search_api_autocomplete\suggester\Suggester. See https://www.drupal.org/node/3254186.', E_USER_DEPRECATED);
 
 /**
  * Provides a suggester plugin that retrieves suggestions from the server.
@@ -12,7 +12,7 @@ use Drupal\search_api_solr_autocomplete\Plugin\search_api_autocomplete\suggester
  * The server needs to support the "search_api_autocomplete" feature for this to
  * work.
  *
- * @deprecated in search_api_solr:4.3.0 and is removed from search_api_solr:5.0.0. Use the
+ * @deprecated in search_api_solr:4.2.4 and is removed from search_api_solr:4.3.0. Use the
  *    \Drupal\search_api_solr_autocomplete\Plugin\search_api_autocomplete\suggester\Suggester instead
  *
  * @see https://www.drupal.org/node/3254186
--- src/Plugin/search_api_autocomplete/suggester/Terms.php
+++ src/Plugin/search_api_autocomplete/suggester/Terms.php
@@ -4,12 +4,12 @@ namespace Drupal\search_api_solr\Plugin\search_api_autocomplete\suggester;
 
 use Drupal\search_api_solr_autocomplete\Plugin\search_api_autocomplete\suggester\Terms as TermsOriginal;
 
-@trigger_error('The ' . __NAMESPACE__ . '\Terms is deprecated in search_api_solr:4.3.0 and is removed from search_api_solr:5.0.0. Instead use \Drupal\search_api_solr_autocomplete\Plugin\search_api_autocomplete\suggester\Terms. See https://www.drupal.org/node/3254186.', E_USER_DEPRECATED);
+@trigger_error('The ' . __NAMESPACE__ . '\Terms is deprecated in search_api_solr:4.2.4 and is removed from search_api_solr:4.3.0. Instead use \Drupal\search_api_solr_autocomplete\Plugin\search_api_autocomplete\suggester\Terms. See https://www.drupal.org/node/3254186.', E_USER_DEPRECATED);
 
 /**
  * Provides a suggester that retrieves suggestions from Solr's Terms component.
  *
- * @deprecated in search_api_solr:4.3.0 and is removed from search_api_solr:5.0.0. Use the
+ * @deprecated in search_api_solr:4.2.4 and is removed from search_api_solr:4.3.0. Use the
  *    \Drupal\search_api_solr_autocomplete\Plugin\search_api_autocomplete\suggester\Terms instead
  *
  * @see https://www.drupal.org/node/3254186
