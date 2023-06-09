1ef251276a4ab01591891fcb9b4d7b4457d20bec
gitressa <3491208+gitressa@users.noreply.github.com>
2023-06-10 01:35:50 +0200

    Update SearchApiSolrCommands.php

--- src/Commands/SearchApiSolrCommands.php
+++ src/Commands/SearchApiSolrCommands.php
@@ -88,7 +88,7 @@ class SearchApiSolrCommands extends DrushCommands implements StdinAwareInterface
    *
    * @default $options []
    *
-   * @usage drush search-api-solr:get-server-config server_id config-solr_8-11-2.zip 8.11.2
+   * @usage drush search-api-solr:get-server-config server_id file_name solr_version
    * Get the config files for a solr server and save it as zip file.
    *
    * @aliases solr-gsc,sasm-gsc,search-api-solr-get-server-config,search-api-solr-multilingual-get-server-config
